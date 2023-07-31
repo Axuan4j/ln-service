444c62542cda33c1dff24f8d807c25eec41bb7a2
WuXuan <508168274@qq.com>
2023-07-31 11:01:28 +0800

    Oauth2 server增加spring redis session

--- ln-service-authorization/src/main/java/com/wu/ln/authorization/LnServiceAuthorizationApplication.java
+++ ln-service-authorization/src/main/java/com/wu/ln/authorization/LnServiceAuthorizationApplication.java
@@ -5,12 +5,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 //@EnableGatewayCommunication
 @EnableServiceExceptionHandler
 @MapperScan("com.wu.ln.authorization.dao")
+@EnableRedisHttpSession
 public class LnServiceAuthorizationApplication {
 
     public static void main(String[] args) {
